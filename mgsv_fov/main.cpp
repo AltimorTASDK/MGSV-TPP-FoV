114ceffef0ce41b6b8e4c5d139365d08cb57bcaa
AltimorTASDK <altimormc@gmail.com>
2015-09-07 04:40:40 -0400

    Now works on the beta

--- mgsv_fov/main.cpp
+++ mgsv_fov/main.cpp
@@ -93,7 +93,7 @@ void __fastcall hook_update_fov_lerp(const uintptr_t thisptr)
  * Scan for the signature of the function call, save the original address and
  * patch the offset to a call to our hook
  */
-void __stdcall hook_decrypt(const char rsp_delta[0x48])
+void hook_decrypt()
 {
 	const auto update_fov_lerp_ref = (int32_t*)(sigscan(
 		"mgsvtpp.exe",
@@ -116,16 +116,22 @@ void __stdcall hook_decrypt(const char rsp_delta[0x48])
 	VirtualProtect((void*)(update_fov_lerp_ref), 4, PAGE_EXECUTE_READWRITE, &old_protect);
 	*update_fov_lerp_ref -= patch_size;
 	VirtualProtect((void*)(update_fov_lerp_ref), 4, old_protect, &old_protect);
+}
 
+__declspec(naked) void hook_decrypt_wrap()
+{
+	_asm call hook_decrypt
+
+	// Do the instructions at the end of the original function
 	_asm mov eax, 1
 	_asm add rsp, 0x48
+	_asm retn
 }
 
 /**
- * DllMain - Calculate camera lens focal lengths from the setting and hook code
- * decryption
+ * DllMain - Entry point
  *
- * Insert a JMP at the end of the decryption routine
+ * Calculate camera lens focal lengths from the setting and hook code decryption
  */
 BOOL WINAPI DllMain(
 	_In_ HINSTANCE hinstDLL,
@@ -140,27 +146,38 @@ BOOL WINAPI DllMain(
 	float tpp_fov;
 	config >> tpp_fov;
 
-	constexpr auto deg2rad     = 180.F / 3.1415926F;
+	constexpr auto deg2rad     = 3.1415926F / 180.F;
 	constexpr auto frame_width = 36.F;
-	const auto tpp_fov_tan     = tan(tpp_fov / deg2rad / 2.F);
+	const auto tpp_fov_tan     = tan(tpp_fov * deg2rad / 2.F);
 	new_tpp_fov      = frame_width / tpp_fov_tan / 2.F;
 	new_shoulder_fov = frame_width / (tpp_fov_tan * (default_tpp_fov / default_shoulder_fov)) / 2.F;
 	new_hiding_fov   = frame_width / (tpp_fov_tan * (default_tpp_fov / default_hiding_fov)) / 2.F;
 	new_cqc_fov      = frame_width / (tpp_fov_tan * (default_tpp_fov / default_cqc_fov)) / 2.F;
 	
-	const auto decrypt_code_end = sigscan(
+	const auto decrypt_code_end_jnb = (int32_t*)(sigscan(
 		"mgsvtpp.exe",
 		"\xD1\xE2\x0F\xAF\xCA\x31\xC8\x8B\x4C\x24\x1C",
-		"xxxxxxxxxxx") + 0x18;
+		"xxxxxxxxxxx") - 0x279);
+
+	// Write hook code into int3 padding
+	const auto int3 = sigscan(
+		"mgsvtpp.exe",
+		"\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC",
+		"xxxxxxxxxxxx");
 
 	// Store hook address in RAX because x64 has no 64 bit address CALL instruction
 	constexpr auto patch_size = 12;
 	DWORD old_protect;
-	VirtualProtect((void*)(decrypt_code_end), patch_size, PAGE_EXECUTE_READWRITE, &old_protect);
-	*(uint16_t*)(decrypt_code_end) = 0xB848; // mov rax, hook_decrypt
-	*(void**)(decrypt_code_end + 2) = hook_decrypt;
-	*(uint16_t*)(decrypt_code_end + 10) = 0xE0FF; // jmp rax
-	VirtualProtect((void*)(decrypt_code_end), patch_size, old_protect, &old_protect);
+	VirtualProtect((void*)(int3), patch_size, PAGE_EXECUTE_READWRITE, &old_protect);
+	*(uint16_t*)(int3) = 0xB848; // mov rax, hook_decrypt
+	*(void**)(int3 + 2) = hook_decrypt_wrap;
+	*(uint16_t*)(int3 + 10) = 0xE0FF; // jmp rax
+	VirtualProtect((void*)(int3), patch_size, old_protect, &old_protect);
+
+	// Replace the jnb to the end of the decryption code with a jnb to our hook
+	VirtualProtect(decrypt_code_end_jnb, 4, PAGE_EXECUTE_READWRITE, &old_protect);
+	*decrypt_code_end_jnb = (int32_t)((intptr_t)(int3) - (intptr_t)(decrypt_code_end_jnb + 1));
+	VirtualProtect(decrypt_code_end_jnb, 4, old_protect, &old_protect);
 
 	return TRUE;
 }
