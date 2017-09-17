61a130d56e4c532e66a74615d7f20f6d9c93c09f
William Toohey <will@mon.im>
2017-09-17 23:19:34 +1000

    Fix for 1.12

--- mgsv_fov/hook_wrapper.asm
+++ mgsv_fov/hook_wrapper.asm
@@ -12,10 +12,30 @@ hook_decrypt_wrap proc
 hook_decrypt_wrap endp
 
 hook_update_fov_lerp_wrap proc
-	push rcx
-	call hook_update_fov_lerp
-	pop rcx
-	ret
+	push rcx				  ; 1 byte
+	call hook_update_fov_lerp ; 5 bytes
+	pop rcx					  ; 1 byte
+
+	; for the instructions we overwrote with our trampoline
+	; MUST match patch size!
+	; make sure trampoline size lies on instruction boundary in original code
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+
+	; will be overwritten with our return address
+	; instruction start + 2 bytes for address
+	mov rax, 1234567890ABCDEFh
+	jmp rax
 hook_update_fov_lerp_wrap endp
 
 end
